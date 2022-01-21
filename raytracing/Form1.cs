3466fa99fc3e66e87f12d17edafab7ebba73149f
m1cha1s <msulej04@gmail.com>
2022-01-21 10:39:02 +0100

    Dodaj pliki projektów.

---
+++ raytracing/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace raytracing
+{
+    public partial class Form1 : Form
+    {
+        List<Sphere> spheres = new List<Sphere>();
+
+        Bitmap bmp;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            spheres.Add(new Sphere(new Vector3(50, 30, 5), 20, Color.Red));
+            spheres.Add(new Sphere(new Vector3(50, 70, 40), 50, Color.Blue));
+            spheres.Add(new Sphere(new Vector3(50, 100, 1), 30, Color.Green));
+
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            g.DrawImage(bmp, 0, 0);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            spheres[1].CPos.Z -= 5 / 30;
+
+            for (int x = 0; x < bmp.Width; x++)
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    bool intersect = false;
+                    float minz = 0;
+                    Color color = Color.Black;
+
+                    foreach (Sphere sph in spheres)
+                    {
+                        (bool inter, float z) = sph.Intersects(new Vector2((float)x, (float)y));
+                        if (inter)
+                        {
+                            if (intersect && z >= minz)
+                                continue;
+
+                            intersect = inter;
+                            minz = z;
+                            color = sph.Col;
+                        }
+                    }
+
+                    if (intersect)
+                    {
+                        bmp.SetPixel(x, y, color);
+                        //pictureBox1.Refresh();
+
+                    }
+                }
+
+            pictureBox1.Refresh();
+        }
+    }
+}
---
+++ raytracing/Sphere.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.Numerics;
+
+namespace raytracing
+{
+    class Sphere
+    {
+        public Vector3 CPos; // position of sphere center
+        public float R;      // sphere radius
+        public Color Col;    // sphere color
+
+        public Sphere(Vector3 cPos, float r, Color col)
+        {
+            CPos = cPos;
+            R = r;
+            Col = col;
+        }
+
+        public (bool, float) Intersects (Vector2 pos)
+        {
+            bool intersects = false;
+            float intersectZ = 0;
+           
+            if (Vector2.DistanceSquared(pos, new Vector2(CPos.X, CPos.Y)) <= R * R)
+            {
+                intersects = true;
+                intersectZ = CPos.Z - MathF.Sqrt(R*R-(pos.X-CPos.X)*(pos.X-CPos.X)-(pos.Y-CPos.Y)*(pos.Y-CPos.Y));
+            }
+            return (intersects, intersectZ);
+        }
+    }
+}
